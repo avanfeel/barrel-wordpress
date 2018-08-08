a417f971074ea05cd575f19425a532eb01f39cff
Wil Bunge <wil@avanfeel.com>
2018-08-08 16:17:35 -0300

    no message

---
+++ versions/0.0.1/wp-content/themes/barrel-base/modules/post/post.php
@@ -0,0 +1,87 @@
+
+<?php
+
+
+	$cat = get_the_category()[0];
+
+	switch ($cat->slug) {
+		case 'recipe':
+			$type = $cat->slug;
+			break;
+		
+		default:
+			$type = 'article';
+			break;
+	}
+
+
+	if(is_front_page()):
+		$class = "type-$type post".($number>0?' hidden':'');
+	else:
+		$class = "";
+	endif; ?>
+
+<article class="<?php echo $class;  ?>" data-module="post">
+
+
+  <header>
+
+
+<?php if(is_front_page()): ?>
+
+    
+
+  	<?php
+
+		$image = get_the_post_thumbnail_url( get_the_ID(), 'medium' );
+
+		the_module('image', array(
+			'image' => $image
+		));
+		?>
+
+		<div class="post__text">
+
+			<span class="__icon-wrapper"><span class="__icon"></span></span>	
+
+			<span class="post__date"><?php echo get_the_date('F d'); ?></span>
+
+			<h1><?php the_title(); ?></h1>
+
+<?php if($number == 0):  ?>
+			<p><?php the_excerpt(); ?></p>
+<?php endif; ?>
+
+    <a class="post__readmore" href="<?php the_permalink(); ?>"></a>
+
+
+    	</div>
+
+
+<?php else: ?>
+
+	<h1><?php the_title(); ?></h1>
+
+	<?php
+
+	the_post_thumbnail();	?>
+
+	
+
+<?php endif; ?>
+
+
+  </header>
+
+
+
+<?php if(is_single()): ?>
+  <main>
+    <?php the_content(); ?>
+  </main>
+
+  <footer></footer>
+
+<?php endif; ?>
+
+</article>
---
+++ versions/0.0.6/wp-content/themes/barrel-base/modules/header/header.js
@@ -0,0 +1,211 @@
+/**
+ * Initializes the site header.
+ * @constructor
+ * @param {Object} el - The site header element.
+ */
+
+
+let loading=false;
+let load_more=true;
+
+
+function init(){
+
+	var $modal = document.querySelector('.modal');
+	var $modal__box = $modal.querySelector('.modal__box');
+	var $modal__box__close = $modal__box.querySelector('.modal__box__close');
+	
+
+	// Set scroll watcher
+	window.onscroll = (y => { scrollWatcher(y) });
+
+
+
+
+	// Close the modal box when close button is clicked
+	$modal__box__close.addEventListener(
+		'click',
+
+		e => {
+			e.preventDefault();
+			closeModal();
+			return false;
+		}
+	);
+
+	// Close modal when background is clicked
+	$modal.addEventListener(
+		'click',
+
+		e => {
+			e.preventDefault();
+			closeModal();
+			return false;
+		}
+	);
+
+	// Prevent modal box to close when its clicked inside
+	$modal__box.addEventListener(
+		'click',
+
+		e => {
+			e.stopPropagation();
+			return false;
+		}
+
+	);
+
+
+	loadPosts();
+
+	// Reveal content
+	document.body.style.display = 'block';
+
+
+}
+
+
+
+/**
+ * Close modal
+ */
+function closeModal(){
+	var $modal = document.querySelector('.modal');
+	$modal.classList.remove('on');
+	$modal.classList.remove('mode-edit');
+	$modal.classList.remove('mode-view');
+	document.body.classList.remove('frozen');
+}
+
+
+
+/**
+ * Watches the sroll position in order to lazyload articles
+ */
+function scrollWatcher(){
+
+	var margin = 100;
+
+	var y = window.innerHeight + window.scrollY
+
+	var body_class = y > 10 ? '' : '';
+
+	var body = document.body,
+    html = document.documentElement;
+
+	var total_document_height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+	if(y > total_document_height - margin){
+		loadPosts();
+	}
+
+	
+
+}
+
+
+
+function appendPosts($posts){
+	// document.querySelector('.articles').innerHTML += html;
+
+	$posts.forEach(
+		$a => {
+
+			$a.querySelector('.__read-more').addEventListener(
+			'click',
+
+			e => {
+				e.preventDefault();
+				modal(id, 'view');
+				return false;
+			}
+		);
+
+			document.querySelector('.articles').appendChild($a);
+		}
+	);
+
+
+	// document.querySelector('.articles')
+
+}
+
+
+function loadPosts(){
+
+	if(!load_more || loading) return;
+
+	loading = true;
+
+	document.querySelector('.articles').classList.add('loading');
+
+	document.body.classList.add('loading');
+
+	let n=0;
+
+	if(document.querySelectorAll('.post.hidden').length==0) load_more=false;
+
+
+	if(window.innerWidth < 1000){
+		var render_count=2;
+	}else{
+		var render_count=3;
+	}
+
+
+
+	setTimeout(
+
+		() => {
+
+			document.querySelectorAll('.post.hidden').forEach(
+
+				$post => {
+
+
+
+					if(++n>render_count) return;
+
+					$post.classList.remove('hidden');
+
+				}
+
+			);
+
+			document.querySelector('.articles').classList.remove('loading');
+			document.body.classList.remove('loading');
+
+			loading = false;
+
+		},
+
+		1000
+
+	);
+
+	
+
+}
+
+
+
+
+
+function Header (el) {
+  this.el = el
+  console.warn('Initialized `Header` module.')
+
+
+
+  init();
+
+
+
+
+
+
+
+
+}
+
+export default Header
